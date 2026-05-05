af8c7c67c5b1bc3d3e04ff3281c743b6b0f89533
hcchien <hcchien@gmail.com>
2026-05-05 23:27:25 +0800

    feat: add external URL launcher boundary

--- ansible_node/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ansible_node/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import connectivity_plus
 import flutter_secure_storage_macos
 import local_auth_darwin
 import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
